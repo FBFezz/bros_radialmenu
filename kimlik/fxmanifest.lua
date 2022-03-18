0aaf0e360183be86638ce51b8a171f53628aa942
Fezz <43589668+FBFezz@users.noreply.github.com>
2022-03-18 02:04:01 +0000

    Update and rename __resource.lua to fxmanifest.lua

--- kimlik/fxmanifest.lua
+++ kimlik/fxmanifest.lua
@@ -1,15 +1,14 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+game 'gta5'
 
 ui_page 'html/index.html'
 
-server_script {
+server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'server.lua'
 }
 
-client_script {
-	'client.lua'
-}
+client_script 'client.lua'
 
 files {
 	'html/index.html',
